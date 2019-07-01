1a8b7b5904669a52570c1ca20f38365bd32c9bc9
Niranjana <35516896+NeeruRamesh@users.noreply.github.com>
2019-07-01 14:35:11 +0530

    This is just another calculator.
    Simple calculator but not limited to just the basics. Most of the scripts don't make use of library functions, because apparently, you make a calculator without using in-built functions, but I guess those are there for a reason :)

---
+++ Scripts/calculator.py
@@ -0,0 +1,45 @@
+import re
+
+print("Welcome to Calcy\n")
+print("You can perform almost all the operations.\nJust keep evaluating results continuously.\nIf you want to calculate new set of results,\nclear the old results by typing 'Clear'\n");
+print("Type 'QUIT' to exit\n")
+
+prev = 0
+run = True
+
+def calculate():
+    global run
+    global prev
+    try:
+        eqn = ""     
+        if prev == 0:
+            eqn = input("Enter the expression:\t")
+        else:
+            eqn = input(str(prev))
+            print(" ")
+
+     
+        if eqn == 'QUIT' or eqn == 'quit' or eqn == 'Quit':
+           print("\nGoodbye :)")
+           run = False
+
+        if prev == 0:
+           prev = eval(eqn)
+
+        else:   
+            prev = eval(str(prev)+eqn)   
+          
+    except:
+        if eqn == 'clear' or eqn == 'Clear' or eqn == 'CLEAR':
+            eqn = ""
+            prev = 0
+            calculate()
+            
+        elif eqn == 'QUIT' or eqn == 'quit' or eqn == 'Quit':
+            print("\nGoodbye :)")
+            run = False
+
+        else:
+            print("\n***Please Enter a valid expression***\n\nType 'Clear' to Clear previous results\n\nType 'QUIT' to exit\n")
+while(run):
+    calculate()
